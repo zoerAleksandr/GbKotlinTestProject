ac4f8f4d58d68a4a4d17e82c780afb11784d5f0e
Aleksandr <79718551+zoerAleksandr@users.noreply.github.com>
2021-07-26 00:56:50 +0300

    implementation clickListener

--- app/src/main/java/com/example/gbkotlintestproject/MainActivity.kt
+++ app/src/main/java/com/example/gbkotlintestproject/MainActivity.kt
@@ -2,11 +2,56 @@ package com.example.gbkotlintestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.gbkotlintestproject.R
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val cityName = findViewById<MaterialTextView>(R.id.city_name)
+        cityName.text = "Москва"
+        cityName.setOnClickListener {
+            Toast.makeText(
+                    this@MainActivity,
+                    "Выбрать город",
+                    Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val temperatureNow = findViewById<MaterialTextView>(R.id.temperature_now)
+        temperatureNow.text = "15"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.setting -> {
+            Toast.makeText(
+                    this@MainActivity,
+                    "Settings",
+                    Toast.LENGTH_SHORT
+            ).show()
+            true
+        }
+
+/*
+* ВОПРОС!
+* Не нашел как реализовать onOptionsItemSelected без else с super методом.
+* Зачем он нужен и как работает?
+* Возможна ли другая реализация?
+* */
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
